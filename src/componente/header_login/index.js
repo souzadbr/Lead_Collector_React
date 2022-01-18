816800babc0a17e5217d1059b5d1d0821e771acb
Debora Souza <deboral.p@hotmail.com>
2022-01-18 17:26:50 -0300

    Adicionar fetch na pagina de login

--- src/componente/header_login/index.js
+++
@@ -1,8 +0,0 @@
-import react from "react";
-
-const Header = ({title})=> (
-    <header>
-        <h1 className="text-center">{title} </h1>
-    </header>
-)
-export default Header
--- src/componente/pages_login/index.js
+++ src/componente/pages_login/index.js
@@ -3,6 +3,42 @@ import {Form, FormGroup, Label, Input, Button}from 'reactstrap'
 import Header from "../header_login";
 
 export default class Login extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            message : this.props.state?this.props.state.message: '',
+        };
+    }   
+
+    signIn = () => {
+        const url = "http://localhost:8080/login";
+        const data = {
+            email: this.email,
+            senha: this.password,
+        };
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+        };
+        fetch(url, requestInfo)
+        .then(response => {
+            if(response.ok){
+                console.log("O login foi realizado com sucesso.")
+                return response.headers.get("Authorization")
+            }
+            throw new Error("Login inválido")
+        }).then(token => {
+            localStorage.setItem('token', token);
+            this.props.history.push("/dashboard");
+        }).catch( e => {
+            this.setState({message: e.message})
+            console.log(this.email, this.password)
+        });
+    }
+
     render (){
         return(
             <div>
